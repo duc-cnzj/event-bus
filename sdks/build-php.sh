6205c44a30a9eadb32f1a6e49dc13d26120ce034
duc <1025434218@qq.com>
2020-11-22 04:12:35 +0800

    wip

--- sdks/build-php.sh
+++ sdks/build-php.sh
@@ -4,4 +4,6 @@ if [ ! -d ./php/src ]; then
   mkdir ./php/src
 fi
 protoc --proto_path=../protos/  --proto_path=. --php_out=./php/src --grpc_out=./php/src --plugin=protoc-gen-grpc=/usr/local/bin/grpc_php_plugin ../protos/*.proto
-cd ./php && composer update && ./vendor/bin/rpc-generator $(pwd)
+
+cd ./php &&  cp composer.json src/DucCnzj && cd src/DucCnzj && mv EventBus src
+composer update && ./vendor/bin/rpc-generator $(pwd)
