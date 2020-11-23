29f957667894b7da58c37e016913c6d7e7303101
duc <1025434218@qq.com>
2020-11-23 14:27:20 +0800

    wip

--- adapter/gorm_logger.go
+++ adapter/gorm_logger.go
@@ -10,24 +10,24 @@ import (
 type GormLoggerAdapter struct {
 }
 
-func (g GormLoggerAdapter) LogMode(level logger.LogLevel) logger.Interface {
+func (g *GormLoggerAdapter) LogMode(level logger.LogLevel) logger.Interface {
 	return g
 }
 
-func (g GormLoggerAdapter) Info(ctx context.Context, s string, i ...interface{}) {
+func (g *GormLoggerAdapter) Info(ctx context.Context, s string, i ...interface{}) {
 	log.WithContext(ctx).WithField("data", i).Info(s)
 }
 
-func (g GormLoggerAdapter) Warn(ctx context.Context, s string, i ...interface{}) {
+func (g *GormLoggerAdapter) Warn(ctx context.Context, s string, i ...interface{}) {
 	log.WithContext(ctx).WithField("data", i).Warning(s)
 
 }
 
-func (g GormLoggerAdapter) Error(ctx context.Context, s string, i ...interface{}) {
+func (g *GormLoggerAdapter) Error(ctx context.Context, s string, i ...interface{}) {
 	log.WithContext(ctx).WithField("data", i).Error(s)
 }
 
-func (g GormLoggerAdapter) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
+func (g *GormLoggerAdapter) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
 	sql, rowsAffected := fc()
 
 	if err != nil {
