786436cd100ca6a2b959432250ee16fdd97a1245
duc <1025434218@qq.com>
2020-12-01 10:04:51 +0800

    wip

--- bootstrapers/db.go
+++ bootstrapers/db.go
@@ -36,7 +36,7 @@ func (D *DBLoader) Boot(app *App) {
 	sqlDB.SetMaxIdleConns(10)
 
 	// SetMaxOpenConns 设置打开数据库连接的最大数量。
-	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetMaxOpenConns(30)
 
 	// SetConnMaxLifetime 设置了连接可复用的最大时间。
 	sqlDB.SetConnMaxLifetime(time.Hour)
